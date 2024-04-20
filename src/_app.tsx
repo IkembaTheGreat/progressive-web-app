7d534a2790381e5a27946a3136beb4d0789e0f1c
Oluwatimilehin <adeoluoyewo13@gmail.com>
2024-04-20 12:31:51 +0100

    Use global.css

--- src/_app.tsx
+++ src/_app.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import "./globals.css"
+import '../globals.css'
 import { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
